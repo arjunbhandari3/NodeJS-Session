74e0572f6652408c5a5f14b5bca98961872f36a3
Arjun Bhandari <arjunbhandari3773@gmail.com>
2022-01-25 16:24:45 +0545

    day 4 tasks added

---
+++ day4/server.js
@@ -0,0 +1,138 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+const router = express.Router();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+router.get("/", (req, res) => res.send("Welcome"));
+
+router.get("/login", (req, res) => {
+  console.log(req.params);
+  res.sendFile(__dirname + "/login.html");
+});
+
+router.post("/login", (req, res) => {
+  console.log(req.body);
+  const { email, password } = req.body;
+  if (email === "test@email.com" && password === "test") {
+    res.status(200).send({ email, password });
+  } else {
+    res.send("Invalid email or password");
+  }
+});
+
+router.get("/tasks", (req, res) => {
+  fs.readFile(__dirname + "/tasks.json", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const tasks = JSON.parse(data);
+      res.send(tasks);
+    }
+  });
+});
+
+router.get("/viewtasks", (req, res) => {
+  res.sendFile(__dirname + "/tasks.html");
+});
+
+router.get("/addtasks", (req, res) => {
+  res.sendFile(__dirname + "/addTask.html");
+});
+
+router.post("/addTasks", (req, res) => {
+  const { title, description, status } = req.body;
+  const newTask = {
+    title,
+    description,
+    status,
+  };
+
+  fs.readFile(__dirname + "/tasks.json", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const tasks = JSON.parse(data);
+      const duplicateTitle = tasks.find((task) => task.title === title);
+      if (duplicateTitle) {
+        res.send("Task already exists");
+      } else {
+        tasks.push(newTask);
+        fs.writeFile(
+          __dirname + "/tasks.json",
+          JSON.stringify(tasks),
+          (err) => {
+            if (err) {
+              console.log(err);
+            } else {
+              res.send({ message: "Task added successfully", data: tasks });
+            }
+          }
+        );
+      }
+    }
+  });
+});
+
+router.put("/tasks/:index", (req, res) => {
+  const index = req.params.index;
+  const { title, description, status } = req.body;
+  console.log(index);
+  console.log(req.body);
+  fs.readFile(__dirname + "/tasks.json", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const tasks = JSON.parse(data);
+      const duplicateTitle = tasks.find((task) => task.title === title);
+      if (duplicateTitle) {
+        res.send("Task already exists");
+      } else {
+        tasks[index].title = title;
+        tasks[index].description = description;
+        tasks[index].status = status;
+        fs.writeFile(
+          __dirname + "/tasks.json",
+          JSON.stringify(tasks),
+          (err) => {
+            if (err) {
+              console.log(err);
+            } else {
+              res.send({ message: "Task updated successfully", data: tasks });
+            }
+          }
+        );
+      }
+    }
+  });
+});
+
+router.delete("/tasks/:index", (req, res) => {
+  const index = req.params.index;
+  fs.readFile(__dirname + "/tasks.json", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const tasks = JSON.parse(data);
+      tasks.splice(index, 1);
+      fs.writeFile(__dirname + "/tasks.json", JSON.stringify(tasks), (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send({ message: "Task deleted successfully", data: tasks });
+        }
+      });
+    }
+  });
+});
+
+app.use("/", router);
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
